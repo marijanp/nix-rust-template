7c7047bb1881d102d208acd1b04c31c60e332ef9
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-09-18 21:29:40 +0200

    server: use database query when handling get_items

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -2,28 +2,25 @@ use askama::Template;
 use axum::response::Html;
 use axum::extract::State;
 use axum_extra::routing::TypedPath;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
 use tracing::instrument;
 
-use crate::item::{Item, ItemsTemplate};
-
-lazy_static! {
-    static ref ITEMS: Mutex<Vec<Item>> = Mutex::new(vec![Item {
-        id: uuid::Uuid::new_v4(),
-        name: "test".to_string(),
-        price: 12345
-    }]);
-}
 use crate::app_state::AppState;
+use crate::db;
+use crate::item::ItemsTemplate;
 
 #[derive(TypedPath, Debug, Clone, Copy)]
 #[typed_path("/api/v1/items")]
 pub struct GetItemsPath;
 
 #[instrument(level = "trace", ret)]
-    let items = ITEMS.lock().unwrap();
 pub async fn get_items_handler(_: GetItemsPath, state: State<AppState>) -> Html<String> {
+    let items = db::get_items(&state.db_pool)
+        .await
+        .map_err(|err| {
+            tracing::error!("{err}");
+            err
+        })
+        .unwrap_or_default();
     let template = ItemsTemplate { items: &items };
     Html(template.render().unwrap())
 }
